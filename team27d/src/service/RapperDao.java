2215bc608c592520971b789bd396f35408a2817f
201-02 <201-02@201-02-PC>
2018-02-08 16:29:45 +0900

    [유국화] 클래스파일-메서드 수정중입니다..일단 커밋

--- team27d/src/service/RapperDao.java
+++ team27d/src/service/RapperDao.java
@@ -3,8 +3,14 @@ package service;
 public class RapperDao { //CRUD À¯±¹È­
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public Rapper selectRapperList(Rapper rap) {
+		rap.getRapperId();
+		rap.getRapperName();
+		rap.getRapperAge();
+		return rap;
 	}
 
 }

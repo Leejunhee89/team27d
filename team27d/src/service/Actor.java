289686d683e08a767b840f25bdebe3add7e9fb20
smart-202-14 <smart-202-14@smart-202-14-PC>
2018-02-08 16:16:53 +0900

    [이준희] Actor.java, Actress.java 수정

--- team27d/src/service/Actor.java
+++ team27d/src/service/Actor.java
@@ -1,5 +1,26 @@
 package service;
 
 public class Actor {
-
+	private int actor_id;
+	private String actor_name;
+	private int actor_age;
+	public int getActor_id() {
+		return actor_id;
+	}
+	public void setActor_id(int actor_id) {
+		this.actor_id = actor_id;
+	}
+	public String getActor_name() {
+		return actor_name;
+	}
+	public void setActor_name(String actor_name) {
+		this.actor_name = actor_name;
+	}
+	public int getActor_age() {
+		return actor_age;
+	}
+	public void setActor_age(int actor_age) {
+		this.actor_age = actor_age;
+	}
+	
 }
---
+++ team27d/src/service/Actress.java
@@ -0,0 +1,26 @@
+package service;
+
+public class Actress {
+	private int actress_id;
+	private String actress_name;
+	private int actress_age;
+	public int getActress_id() {
+		return actress_id;
+	}
+	public void setActress_id(int actress_id) {
+		this.actress_id = actress_id;
+	}
+	public String getActress_name() {
+		return actress_name;
+	}
+	public void setActress_name(String actress_name) {
+		this.actress_name = actress_name;
+	}
+	public int getActress_age() {
+		return actress_age;
+	}
+	public void setActress_age(int actress_age) {
+		this.actress_age = actress_age;
+	}
+	
+}

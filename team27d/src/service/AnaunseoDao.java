b5ab7d47717914d0888d489737fd32fd43af5fb3
201-12 <201-12@201-12-PC>
2018-02-08 17:11:03 +0900

    [이춘림] AnaunseoDao.java anaInsert 메서드 추가

--- team27d/src/service/AnaunseoDao.java
+++ team27d/src/service/AnaunseoDao.java
@@ -19,8 +19,36 @@ public class AnaunseoDao {
 		
 		conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
 		
+		if (pstmt != null) { pstmt.close(); } 
+		
 		return conn;
 	}
+	
+	public void AnaInsert(Anaunseo ana) throws ClassNotFoundException, SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+	
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev?" +
+				"useUnicode=true&characterEncoding=euckr";
+		String dbUser = "root";
+		String dbPass = "java0000";
+		
+		conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+		
+		pstmt = conn.prepareStatement("INSERT INTO Anaunseo VALUES(?, ?, ?)");
+		pstmt.setString(1, ana.getAnaunseoId());
+		pstmt.setString(2, ana.getAnaunseoName());
+		pstmt.setString(3, ana.getAnaunseoAge());
+		
+		pstmt.executeUpdate();
+	
+		if (pstmt != null) { pstmt.close(); } 
+		if (conn != null) { conn.close(); }
+	}
+	
 	
 	
 }

ad3450a15fc11cd052ff25ce753ebd1c8d9eb2b3
201-12 <201-12@201-12-PC>
2018-02-08 16:26:00 +0900

    [이춘림] 아나운서 클래스 완성하고 , 아나운서 Dao jsp파일 만들어만 놓았음

---
+++ team27d/src/service/Anaunseo.java
@@ -0,0 +1,28 @@
+package service;
+
+public class Anaunseo {
+	private String anaunseoId;
+	private String anaunseoName;
+	private String anaunseoAge;
+	
+	public String getAnaunseoId() {
+		return anaunseoId;
+	}
+	public void setAnaunseoId(String anaunseoId) {
+		this.anaunseoId = anaunseoId;
+	}
+	public String getAnaunseoName() {
+		return anaunseoName;
+	}
+	public void setAnaunseoName(String anaunseoName) {
+		this.anaunseoName = anaunseoName;
+	}
+	public String getAnaunseoAge() {
+		return anaunseoAge;
+	}
+	public void setAnaunseoAge(String anaunseoAge) {
+		this.anaunseoAge = anaunseoAge;
+	}
+
+	
+}

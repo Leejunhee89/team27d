151b262015a5c3b7fb47b5805e6eb8fadc871380
smart-202-5 <smart-202-5@smart-202-5-PC>
2018-02-09 08:48:39 +0900

    [방민영] Comedian.java ComedianDao.java comedianList.jsp 수정했습니다.

--- team27d/src/service/ComedianDao.java
+++ team27d/src/service/ComedianDao.java
@@ -1,5 +1,49 @@
 package service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class ComedianDao {
 
+	public ArrayList<Comedian> selectComedianList(){
+		ArrayList<Comedian> arrayComedian = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		Connection connection = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev?useUnicode=true&characterEncoding=euckr";
+			String dbUser = "root";
+			String dbPass = "java0000";
+		
+		
+			connection = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+			preparedStatement = connection.prepareStatement("select comedian_id, comedian_name, comedian_age from comedian");
+			resultSet = preparedStatement.executeQuery();
+		
+			arrayComedian = new ArrayList<Comedian>();
+			
+			while(resultSet.next()) {
+				Comedian comedian = new Comedian();
+				comedian.setComedianId(resultSet.getInt("comedian_id"));
+				comedian.setComedianName(resultSet.getString("comedian_name"));
+				comedian.setComedianAge(resultSet.getInt("comedian_age"));
+				arrayComedian.add(comedian);
+				
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return arrayComedian;
+	}
 }

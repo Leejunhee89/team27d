ce373dbe89b61329d1bf47d5161ae07615388688
smart-202-14 <smart-202-14@smart-202-14-PC>
2018-02-09 09:58:02 +0900

    [이준희] Dao 지금까지 한것

--- team27d/src/service/ActorDao.java
+++ team27d/src/service/ActorDao.java
@@ -1,25 +1,58 @@
 package service;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.sql.DriverManager;
 
 public class ActorDao {
-	Connection conn = null;
-	PreparedStatement pstmt = null;
 	
+	public ArrayList<Actor> selectActor() {
+		ArrayList<Actor> arrayActor = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet result = null;
+		
+		
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev?useUnicode=true&characterEncoding=euckr";
+			String dbUser = "root";
+			String dbPass = "java0000";
 	
-	Class.forName("com.mysql.jdbc.Driver");
-	String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev? useUnicode=true&characterEncoding=euckr";
-	String dbUser = "root";
-	String dbPass = "java0000";
-	conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
-	
-	pstmt = conn.prepareStatement(
-			"INSERT INTO actor VALUES (?, ?, ?)");
-	System.out.println(pstmt + "<-- pstmt 1");
-	pstmt.setString(1, m_id);
-	pstmt.setString(2, m_pw);
-	pstmt.setString(3, m_level);
-	
+			conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+			pstmt = conn.prepareStatement("select actor_id, actor_name, actor_age from actor"); 
+			result = pstmt.executeQuery();
+			
+			arrayActor = new ArrayList<Actor>();
+			while(result.next()) {
+				Actor actor = new Actor();
+				String actorId = result.getString("actor_id");
+				String actorName = result.getString("actor_name");	
+				String actorAge = result.getString("actor_age");
+				
+				
+				
+				
+			}
+			
+		
+			
+		
+		
+		
+		
+		
+		
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return arrayActor;
+	}	
 }

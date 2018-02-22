6f5a681d2f1d23713a4a6729c67575af52f17f61
201-12 <201-12@201-12-PC>
2018-02-22 12:27:10 +0900

    [이춘림] 아나운서 DAO부분 forUpdate(), Update()메서드 작성후 updateForm.jsp, updateAction.jsp 부분에 적용후 작동확인함.

--- team27d/WebContent/updateAnaunseoAction.jsp
+++ team27d/WebContent/updateAnaunseoAction.jsp
@@ -1,12 +1,24 @@
 <!-- team27d [이춘림] -->
 <%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
+<%@ page import = "service.AnaunseoDao" %>
+<%@ page import = "service.Anaunseo" %>
 <!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
-	<title>Insert title here</title>
-</head>
-<body>
-
-</body>
-</html>
+<%
+	request.setCharacterEncoding("euc-kr");
+	String send_id = request.getParameter("anaunseo_id");
+	int anaunseo_id = Integer.parseInt(send_id);
+	String anaunseo_name = request.getParameter("anaunseo_name");
+	String send_age = request.getParameter("anaunseo_age");
+	int anaunseo_age = Integer.parseInt(send_age);
+	
+	AnaunseoDao anaunseoDao = new AnaunseoDao();
+	Anaunseo anaunseo = new Anaunseo();
+	
+	anaunseo.setAnaunseoId(anaunseo_id);
+	anaunseo.setAnaunseoName(anaunseo_name);
+	anaunseo.setAnaunseoAge(anaunseo_age);
+	
+	anaunseoDao.updateAnaunseo(anaunseo);
+	
+	response.sendRedirect(request.getContextPath() + "/announcerList.jsp");
+%>
--- team27d/src/service/AnaunseoDao.java
+++ team27d/src/service/AnaunseoDao.java
@@ -9,6 +9,103 @@ import java.util.ArrayList;
 import service.Anaunseo;
 
 public class AnaunseoDao {
+	Connection connection;
+	PreparedStatement preparedStatement;
+	ResultSet resultSet;
+	
+	public int updateAnaunseo(Anaunseo anaunseo) {
+		int result = 0;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/jjdev?useUnicode=true&characterEncoding=euckr";
+			String id = "root";
+			String pw = "java0000";
+			connection = DriverManager.getConnection(url, id, pw);
+			preparedStatement = connection.prepareStatement("UPDATE anaunseo SET anaunseo_name = ?, anaunseo_age = ? WHERE anaunseo_id = ?");
+			preparedStatement.setString(1, anaunseo.getAnaunseoName());
+			preparedStatement.setInt(2, anaunseo.getAnaunseoAge());
+			preparedStatement.setInt(3, anaunseo.getAnaunseoId());
+			
+			result = preparedStatement.executeUpdate();
+			
+		}catch(SQLException exception) {
+			exception.printStackTrace();
+		}catch(ClassNotFoundException exception) {
+			exception.printStackTrace();
+		}finally {
+			if(preparedStatement != null) {
+				try {
+					preparedStatement.close();
+					preparedStatement = null;
+				}catch(SQLException exception) {
+					exception.printStackTrace();
+				}
+			}
+			if(connection != null) {
+				try {
+					connection.close();
+					connection = null;
+				}catch(SQLException exception) {
+					exception.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+	
+	public Anaunseo anaunseoForUpdate(int anaunseoId) {
+		Anaunseo anaunseo = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/jjdev?useUnicode=true&characterEncoding=euckr";
+			String id = "root";
+			String pw = "java0000";
+			connection = DriverManager.getConnection(url, id, pw);
+			preparedStatement = connection.prepareStatement("SELECT * FROM anaunseo WHERE anaunseo_id = ?");
+			preparedStatement.setInt(1, anaunseoId);
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next()) {
+				anaunseo = new Anaunseo();
+				anaunseo.setAnaunseoId(resultSet.getInt("anaunseo_id"));
+				anaunseo.setAnaunseoName(resultSet.getString("anaunseo_name"));
+				anaunseo.setAnaunseoAge(resultSet.getInt("anaunseo_age"));
+			}
+			
+		}catch(SQLException exception) {
+			exception.printStackTrace();
+		}catch(ClassNotFoundException exception) {
+			exception.printStackTrace();
+		}finally {
+			if(resultSet != null) {
+				try {
+					resultSet.close();
+					resultSet = null;
+				}catch(SQLException exception) {
+					exception.printStackTrace();
+				}
+			}
+			if(preparedStatement != null) {
+				try {
+					preparedStatement.close();
+					preparedStatement = null;
+				}catch(SQLException exception) {
+					exception.printStackTrace();
+				}
+			}
+			if(connection != null) {
+				try {
+					connection.close();
+					connection = null;
+				}catch(SQLException exception) {
+					exception.printStackTrace();
+				}
+			}
+		}
+		return anaunseo;
+	}
 	
 	public int deleteAnaunseo(int anaunseoId) {
 		int result = 0;
